28af9aa68736b637401f87d57c0213b210751370
Mchl37 <71225509+Mchl37@users.noreply.github.com>
2022-04-05 17:03:08 +0200

    Initial commit

---
+++ app/src/main/java/com/mguelin/todomichelguelin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mguelin.todomichelguelin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
